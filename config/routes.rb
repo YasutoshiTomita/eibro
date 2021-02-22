3af59834edf5f6b99b9749eda988dc866b218c58
systm <sy899gtge@gmail.com>
2021-02-23 00:16:24 +0900

    add home/top header

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'movie/show'
+
   get 'post/show'
+
   get 'home/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
